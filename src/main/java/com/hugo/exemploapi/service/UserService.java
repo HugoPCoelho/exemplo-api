c00bab1eb41419e56211bdfbd36fb4eddc78e600
HugoPCoelho <hugo.penha.coelho@gmail.com>
2023-10-21 17:39:19 -0300

    Aplicação da camada service

---
+++ src/main/java/com/hugo/exemploapi/service/UserService.java
@@ -0,0 +1,12 @@
+package com.hugo.exemploapi.service;
+
+import com.hugo.exemploapi.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+
+
+}
